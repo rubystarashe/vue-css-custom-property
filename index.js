8652bd50f52e21171a93a9ffa2788fa97abfa9c8
RubystarAshe <rubystarashe@gmail.com>
2018-10-25 22:34:00 +0900

    [M] ES5

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ export default {
       readyKey: typeof options === 'string' ? options : (options || {}).readyKey || 'customPropertiesReady'
     }
     Vue.mixin({
-      mounted() {
+      mounted: function () {
         const _this = this
         const properties = this.$options.customProperties || {}
-        Object.keys(properties).forEach(key => {
+        Object.keys(properties).forEach(function (key) {
           const observer = properties[key]
-          _this.$watch(observer, value => {
+          _this.$watch(observer, function (value) {
             const style = _this.$el.style
             if (style) {
               style.setProperty(key, value)
@@ -22,7 +22,7 @@ export default {
         if (this.$options.customPropertiesReady) this.$options.customPropertiesReady.apply(this)
       }
     })
-    const functions = (name, value, vue = null) => {
+    function functions (name, value, vue) {
       if (vue) {
         vue.$el.style.setProperty(name, value)
       } else {
